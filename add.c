e6eb276d85a78fba1f6974fab5cc010066dbb045
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2024-01-21 18:48:44 +0100

    monty2

--- add.c
+++ add.c
@@ -1,14 +1,24 @@
 #include "monty.h"
 
+/**
+ * add - Adds the top two elements of the stack.
+ * @stack: Double pointer to the beginning of the stack.
+ * @line_number: Line number being executed.
+ */
 void add(stack_t **stack, unsigned int line_number)
 {
-    if (!*stack || !(*stack)->next)
+    stack_t *current = *stack;
+
+    if (current == NULL || current->next == NULL)
     {
         fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*stack);
         exit(EXIT_FAILURE);
     }
 
-    (*stack)->next->n += (*stack)->n;
-    pop(stack, line_number);
+    current->next->n += current->n;
+    pop(stack, line_number); /* Remove the top element */
 }
 
